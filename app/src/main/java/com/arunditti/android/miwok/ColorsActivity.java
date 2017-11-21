2504d5165db993ce650a5cded21245ce6aeabcd3
arunditti <arunditti@gmail.com>
2017-11-21 10:27:14 -0500

    Used releaseMediaPlayer to release the resources

--- app/src/main/java/com/arunditti/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/arunditti/android/miwok/ColorsActivity.java
@@ -13,6 +13,16 @@ import java.util.ArrayList;
 public class ColorsActivity extends AppCompatActivity {
 
     private MediaPlayer mMediaPlayer;
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +34,14 @@ public class ColorsActivity extends AppCompatActivity {
 
         // Word w = new Word("One", "lutti");
         // words.add(w);
-        words.add(new Word("red", "weṭeṭṭi",R.drawable.color_red, R.raw.color_red));
-        words.add(new Word("green", "chokokki",R.drawable.color_green, R.raw.color_green));
-        words.add(new Word("brown", "ṭakaakki",R.drawable.color_brown, R.raw.color_brown));
-        words.add(new Word("gray", "ṭopoppi",R.drawable.color_gray, R.raw.color_gray));
-        words.add(new Word("black", "kululli",R.drawable.color_black, R.raw.color_black));
-        words.add(new Word("white", "kelelli",R.drawable.color_white, R.raw.color_white));
-        words.add(new Word("dusty yellow", "ṭopiisә",R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
-        words.add(new Word("mustard yellow", "chiwiiṭә",R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
+        words.add(new Word("red", "weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
+        words.add(new Word("green", "chokokki", R.drawable.color_green, R.raw.color_green));
+        words.add(new Word("brown", "ṭakaakki", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new Word("gray", "ṭopoppi", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new Word("black", "kululli", R.drawable.color_black, R.raw.color_black));
+        words.add(new Word("white", "kelelli", R.drawable.color_white, R.raw.color_white));
+        words.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
 
         //Log.v("NumbersActivity", "Word at index 0: " + words);
 
@@ -56,11 +66,49 @@ public class ColorsActivity extends AppCompatActivity {
                 //Get the object at the given position the user clicked on
                 Word word = words.get(position);
 
+                // Release the media player if it currently exists because we are about to
+                // play a different sound file
+                releaseMediaPlayer();
+
+
+
                 //Create and setup the audio resource associated with that world
                 mMediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getAudioResourceId());
                 mMediaPlayer.start();
+
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
             }
         });
+
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // When the activity is stopped, release the media player resources because we won't
+        // be playing any more sounds.
+        releaseMediaPlayer();
+    }
+
+        /**
+         * Clean up the media player by releasing its resources.
+         */
+        private void releaseMediaPlayer() {
+            // If the media player is not null, then it may be currently playing a sound.
+            if (mMediaPlayer != null) {
+                // Regardless of the current state of the media player, release its resources
+                // because we no longer need it.
+                mMediaPlayer.release();
+
+                // Set the media player back to null. For our code, we've decided that
+                // setting the media player to null is an easy way to tell that the media player
+                // is not configured to play an audio file at the moment.
+                mMediaPlayer = null;
+            }
 
     }
 }
--- app/src/main/java/com/arunditti/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/arunditti/android/miwok/NumbersActivity.java
@@ -15,6 +15,18 @@ public class NumbersActivity extends AppCompatActivity {
 
    private MediaPlayer mMediaPlayer;
 
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,15 +35,15 @@ public class NumbersActivity extends AppCompatActivity {
         //Create an ArrayList of words
         final ArrayList<Word> words = new ArrayList<Word>();
 
-       // Word w = new Word("One", "lutti");
-       // words.add(w);
+        // Word w = new Word("One", "lutti");
+        // words.add(w);
         words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
         words.add(new Word("two", "ottiko", R.drawable.number_two, R.raw.number_two));
         words.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
         words.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
         words.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
         words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
-        words.add(new Word("seven", "kenekaku",R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
         words.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
         words.add(new Word("nine", "wo’e", R.drawable.number_nine, R.raw.number_nine));
         words.add(new Word("ten", "na’aacha", R.drawable.number_ten, R.raw.number_ten));
@@ -43,7 +55,7 @@ public class NumbersActivity extends AppCompatActivity {
         // simple_list_item_1.xml layout resource defined in the Android framework.
         // This list item layout contains a single {@link TextView}, which the adapter will set to
         // display a single word.
-       WordAdapter adapter = new WordAdapter(this, words, R.color.category_numbers);
+        WordAdapter adapter = new WordAdapter(this, words, R.color.category_numbers);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
@@ -62,13 +74,46 @@ public class NumbersActivity extends AppCompatActivity {
                 //Get the object at the given position the user clicked on
                 Word word = words.get(position);
 
+                // Release the media player if it currently exists because we are about to
+                // play a different sound file
+                releaseMediaPlayer();
+
                 Log.v("NumbersActivity", "Current word: " + word);
 
                 //Create and setup the audio resource associated with that world
                 mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceId());
                 mMediaPlayer.start();
+
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
             }
         });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // When the activity is stopped, release the media player resources because we won't
+        // be playing any more sounds.
+        releaseMediaPlayer();
+    }
+        /**
+         * Clean up the media player by releasing its resources.
+         */
+        private void releaseMediaPlayer() {
+            // If the media player is not null, then it may be currently playing a sound.
+            if (mMediaPlayer != null) {
+                // Regardless of the current state of the media player, release its resources
+                // because we no longer need it.
+                mMediaPlayer.release();
+
+                // Set the media player back to null. For our code, we've decided that
+                // setting the media player to null is an easy way to tell that the media player
+                // is not configured to play an audio file at the moment.
+                mMediaPlayer = null;
+        }
 
     }
 }
